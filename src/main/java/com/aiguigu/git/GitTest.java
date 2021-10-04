d69d4d5f961c7a37afa0e4a842c9c8c394073583
Layne <Layne@atgugui.com>
2021-10-04 11:16:55 +0800

    first commit

---
+++ src/main/java/com/aiguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.aiguigu.git;
+
+/**
+ * @author yebinbin
+ * @create 2021-10-04 11:11
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+    }
+}
